4da8263411ab38c22ebd66a1bb9e44ee6718e15e
Gabriel Ferreira <103156855+ggabrielferreira@users.noreply.github.com>
2022-06-09 19:48:31 -0300

    Acrescentando script para adicionar palavras ao jogo da forca

---
+++ js/adicionapalavra.js
@@ -0,0 +1,12 @@
+let novaPalavra;
+let palavras = [];
+let p
+
+selecionarElemento('#adicionar').addEventListener('click', function(){
+    novaPalavra = selecionarElemento('#novaPalavra').value; 
+    palavras.push(novaPalavra);
+    selecionarElemento('#novaPalavra').value = "";
+    p = document.createElement('p');
+    p.textContent = novaPalavra;
+    selecionarElemento('#palavras').appendChild(p);
+  });
